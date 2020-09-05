32699bd66844e461914d8e843f8fd2363270b029
agusTrih <ant3dong@gmail.com>
2020-09-05 07:14:38 +0700

    using scss bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
-
+import "./assets/scss/style.scss";
 import Header from "./components/Header/Header";
-
+import Card from "./components/Card/Card";
 function App() {
     return (
         <div className="App">
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+// import "../../assets/css/styles.css";
+export default function Card() {
+    return <div className="card"></div>;
+}
