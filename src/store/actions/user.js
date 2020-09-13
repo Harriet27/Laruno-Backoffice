6e4b04998dc2c323856279efd132c821860c9977
agusTrih <ant3dong@gmail.com>
2020-09-14 04:18:41 +0700

    add post administrator

--- src/store/actions/user.js
+++ src/store/actions/user.js
@@ -1,13 +1,15 @@
-import Swal from "sweetalert2";
-import jwt_decode from "jwt-decode";
+import Swal from 'sweetalert2';
+import jwt_decode from 'jwt-decode';
+
+// login In BackOffice
 const fetchPostLogin = (form, history) => async (dispatch) => {
     try {
         const url = `http://139.162.59.84:7000/api/v1/users/login`;
         const options = {
-            method: "POST",
+            method: 'POST',
             body: JSON.stringify(form),
             headers: {
-                "content-type": "application/json",
+                'content-type': 'application/json',
             },
         };
 
@@ -16,27 +18,62 @@ const fetchPostLogin = (form, history) => async (dispatch) => {
 
         if (response.status === 201) {
             Swal.fire({
-                title: "Berhasil Login!",
-                text: "",
-                icon: "success",
+                title: 'Berhasil Login!',
+                text: '',
+                icon: 'success',
             });
-            localStorage.setItem("user", JSON.stringify(result));
-            history.push("/products");
+            localStorage.setItem('user', JSON.stringify(result));
+            history.push('/products');
         } else if (response.status === 401) {
             localStorage.clear();
         } else {
             Swal.fire({
-                title: "Wrong email!",
-                text: "Email atau kata sandi anda tidak valid",
-                icon: "error",
+                title: 'Wrong email!',
+                text: 'Email atau kata sandi anda tidak valid',
+                icon: 'error',
             });
         }
     } catch (error) {
         Swal.fire({
-            title: "Email atau Password Salah!",
-            text: "",
-            icon: "error",
+            title: 'Email atau Password Salah!',
+            text: '',
+            icon: 'error',
         });
     }
 };
-export { fetchPostLogin };
+
+// Add Administrator for SuperAdmin
+const fetchPostAdministrator = (form, history) => async () => {
+    const token = JSON.parse(localStorage.getItem('user')).accessToken;
+    try {
+        const url = `http://139.162.59.84:7000/api/v1/users/add`;
+        const options = {
+            method: 'POST',
+            body: JSON.stringify(form),
+            headers: {
+                'Content-type': 'application/json',
+                Authorization: `Beare ${token}`,
+            },
+        };
+        const response = await fetch(url, options);
+        const result = await response.json();
+
+        if (response.status === 201) {
+            Swal.fire({
+                title: 'Berhasil Login!',
+                text: '',
+                icon: 'success',
+            });
+            history.push('/');
+        } else {
+            Swal.fire({
+                title: 'Email atau Password Salah!',
+                text: '',
+                icon: 'error',
+            });
+        }
+    } catch (error) {
+        console.log(error);
+    }
+};
+export { fetchPostLogin, fetchPostAdministrator };
