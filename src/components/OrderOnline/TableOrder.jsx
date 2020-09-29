8a934b1a30480fa1a93cc572fe1a6bbc2656fe83
agusTrih <ant3dong@gmail.com>
2020-09-29 16:17:17 +0700

    fix some bug

--- src/components/OrderOnline/TableOrder.jsx
+++ src/components/OrderOnline/TableOrder.jsx
@@ -2,102 +2,97 @@ import React from 'react';
 import { Table } from 'reactstrap';
 import Card from '../../elements/Card/Card';
 import Styled from 'styled-components';
-
-// --- Styled Components --- //
-const Th = Styled.th`
-font-size:  ${(props) => (props.td ? '16px' : '20px')};
-font-weight: ${(props) => (props.td ? 'normal' : '600')};
-text-align: center;
-`;
-// --- Styled Components --- //
+import { Th, Overflow } from '../../elements/Styled/StyledForm';
 
 const TableOrder = (props) => {
     return (
         <Card isNormal>
-            <Table striped>
-                <thead>
-                    <tr>
-                        <Th>Invoice Number</Th>
-                        <Th>Tag</Th>
-                        <Th>Order Date</Th>
-                        <Th>Costumer Name</Th>
-                        <Th>Customer Phone</Th>
-                        <Th>Product</Th>
-                        <Th>Total Price</Th>
-                        <Th>Payment Status</Th>
-                        <Th>Paid At</Th>
-                        <Th>Seller</Th>
-                        <Th>Actions</Th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <Th as="td" td>
-                            One
-                        </Th>
-                        <Th as="td" td>
-                            Two
-                        </Th>
-                        <Th as="td" td>
-                            Three
-                        </Th>
-                        <Th as="td" td>
-                            Four
-                        </Th>
-                        <Th as="td" td>
-                            Five
-                        </Th>
-                        <Th as="td" td>
-                            Six
-                        </Th>
-                        <Th as="td" td>
-                            Seven
-                        </Th>
-                        <Th as="td" td>
-                            Eight
-                        </Th>
-                        <Th as="td" td>
-                            Seven
-                        </Th>
-                        <Th as="td" td>
-                            Eight
-                        </Th>
-                    </tr>
+            <Overflow>
+                <Table striped>
+                    <thead>
+                        <tr>
+                            <Th>Invoice Number</Th>
+                            <Th>Tag</Th>
+                            <Th>Order Date</Th>
+                            <Th>Costumer Name</Th>
+                            <Th>Customer Phone</Th>
+                            <Th>Product</Th>
+                            <Th>Total Price</Th>
+                            <Th>Payment Status</Th>
+                            <Th>Paid At</Th>
+                            <Th>Seller</Th>
+                            <Th>Actions</Th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <Th as="td" td>
+                                One
+                            </Th>
+                            <Th as="td" td>
+                                Two
+                            </Th>
+                            <Th as="td" td>
+                                Three
+                            </Th>
+                            <Th as="td" td>
+                                Four
+                            </Th>
+                            <Th as="td" td>
+                                Five
+                            </Th>
+                            <Th as="td" td>
+                                Six
+                            </Th>
+                            <Th as="td" td>
+                                Seven
+                            </Th>
+                            <Th as="td" td>
+                                Eight
+                            </Th>
+                            <Th as="td" td>
+                                Seven
+                            </Th>
+                            <Th as="td" td>
+                                Eight
+                            </Th>
+                        </tr>
 
-                    <tr>
-                        <Th as="td" td>
-                            One
-                        </Th>
-                        <Th as="td" td>
-                            Two
-                        </Th>
-                        <Th as="td" td>
-                            Three
-                        </Th>
-                        <Th as="td" td>
-                            Four
-                        </Th>
-                        <Th as="td" td>
-                            Five
-                        </Th>
-                        <Th as="td" td>
-                            Six
-                        </Th>
-                        <Th as="td" td>
-                            Seven
-                        </Th>
-                        <Th as="td" td>
-                            Eight
-                        </Th>
-                        <Th as="td" td>
-                            Seven
-                        </Th>
-                        <Th as="td" td>
-                            Eight
-                        </Th>
-                    </tr>
-                </tbody>
-            </Table>
+                        <tr>
+                            <Th as="td" td>
+                                One
+                            </Th>
+                            <Th as="td" td>
+                                Two
+                            </Th>
+                            <Th as="td" td>
+                                Three
+                            </Th>
+                            <Th as="td" td>
+                                Four
+                            </Th>
+                            <Th as="td" td>
+                                Five
+                            </Th>
+                            <Th as="td" td>
+                                Six
+                            </Th>
+                            <Th as="td" td>
+                                Seven
+                            </Th>
+                            <Th as="td" td>
+                                Eight
+                            </Th>
+                            <Th as="td" td>
+                                Seven
+                            </Th>
+                            <Th as="td" td>
+                                Eight
+                            </Th>
+                        </tr>
+                    </tbody>
+                </Table>
+            </Overflow>
         </Card>
     );
 };
