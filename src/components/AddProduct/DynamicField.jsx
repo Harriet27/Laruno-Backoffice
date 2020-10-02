cf7f9f4898b985ee09a2a59e5afca2a2f44d17a5
agusTrih <ant3dong@gmail.com>
2020-10-02 16:04:39 +0700

    add styled in dynamic fields

--- src/components/AddProduct/DynamicField.jsx
+++ src/components/AddProduct/DynamicField.jsx
@@ -1,5 +1,59 @@
 import React, { useState } from 'react';
 import Styled from 'styled-components';
+
+const Input = Styled.input`
+    width: 100%;
+    padding: .375rem;
+    font-size: 14px;
+    font-weight: 400;
+    color: #495057;
+    border-radius: ${(props) => (props.price ? '0' : '3px')};
+    background-color: #FCFCFC;
+    border:${(props) => (props.price ? '0' : '1px solid #ced4da')};
+    &:focus{
+    outline: none !important;
+    border:1px solid #66AFE9;
+    }
+`;
+
+const Section = Styled.section`
+    display: flex;
+    padding: 50px 100px;
+    width: 100%;
+    line-height: 1.5;
+    @media (max-width: 800px) {
+        padding: 20px 40px;
+          }
+`;
+const Label = Styled.label`
+    
+`;
+const WrapsField = Styled.div`
+    margin-bottom: 25px;
+    width: ${(props) => (props.dividedByTwo ? '45%' : null)}
+`;
+const Span = Styled.span`
+    font-weight: bold;
+    color: #656565;
+    font-size: 18px;
+`;
+
+const SectionOne = Styled.div`
+    display: flex;
+    width: 50%;
+    @media (max-width: 800px) {
+        width: 100%
+    }
+`;
+
+const Form = Styled.form`
+    padding: 50px 40px;
+    @media (max-width: 800px) {
+        padding: 20px;
+    }
+`;
+// --- BATAS BAWAH --- //
+
 export default function DynamicField(props) {
     const {
         handleAdd,
@@ -35,14 +89,15 @@ export default function DynamicField(props) {
                             key={`${field}-${idx}`}
                         >
                             <label>Title</label>
-                            <input
+                            <Input
                                 type="text"
                                 name={`one-${idx}`}
                                 placeholder="Enter text"
                                 onChange={(e) => handleChange(idx, e)}
                             />
                             <label>Content</label>
-                            <textarea
+                            <Input
+                                as="textarea"
                                 name={`number-${idx}`}
                                 placeholder="Enter text"
                                 onChange={(e) => handleChangeContents(idx, e)}
