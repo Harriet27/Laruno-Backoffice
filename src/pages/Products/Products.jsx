d6513c33f96366ee3f63fa83a7964dac928a585b
agusTrih <ant3dong@gmail.com>
2020-10-08 14:03:24 +0700

    add total product

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import Styled from 'styled-components';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import { faWallet } from '@fortawesome/free-solid-svg-icons';
-
+import DaTest from '../../components/Product/FileStackTest';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchGetProduct } from '../../store/actions';
 // --- Elements, Pages, Components --- //
 import CardGetData from '../../elements/Card/CardGetData';
 import DataProduct from '../../components/Product/DataProduct';
@@ -16,36 +18,42 @@ const Wraps = Styled.div`
 `;
 
 export default function Products() {
+    const dispatch = useDispatch();
+    const product = useSelector((state) => state.product.getProduct);
+    console.log(product, 'product di product menu');
     return (
         <section style={{ margin: '0 50px' }}>
-            <Wraps>
-                <CardGetData
-                    icon={faShoppingCart}
-                    number="2"
-                    text="Total Products"
-                ></CardGetData>
+            {product !== null && (
+                <Wraps>
+                    <CardGetData
+                        icon={faShoppingCart}
+                        number={product.data.length}
+                        text="Total Products"
+                    ></CardGetData>
 
-                <CardGetData
-                    icon={faWallet}
-                    number="2"
-                    text="Total Paid"
-                ></CardGetData>
+                    <CardGetData
+                        icon={faWallet}
+                        number="2"
+                        text="Total Paid"
+                    ></CardGetData>
 
-                <CardGetData
-                    icon={faShoppingCart}
-                    number="0"
-                    text="Unpaid Orders"
-                ></CardGetData>
+                    <CardGetData
+                        icon={faShoppingCart}
+                        number="0"
+                        text="Unpaid Orders"
+                    ></CardGetData>
 
-                <CardGetData
-                    icon={faShoppingCart}
-                    number="0"
-                    text="Unpaid Orders"
-                ></CardGetData>
-            </Wraps>
+                    <CardGetData
+                        icon={faShoppingCart}
+                        number="0"
+                        text="Unpaid Orders"
+                    ></CardGetData>
+                </Wraps>
+            )}
             {/* --- section 2 --- */}
-            {/* <DataProduct /> */}
-            <DataProductTest />
+            <DataProduct />
+            {/* <DaTest /> */}
+            {/* <DataProductTest /> */}
         </section>
     );
 }
