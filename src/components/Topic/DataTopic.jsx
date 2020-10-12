7cde090fa194d0d879c439f63b87e8f9cce8de3d
agusTrih <ant3dong@gmail.com>
2020-10-12 16:01:33 +0700

    rewrite topic

--- src/components/Topic/DataTopic.jsx
+++ src/components/Topic/DataTopic.jsx
@@ -1,42 +1,81 @@
 import React, { useEffect, useState } from 'react';
+import TablePagination from '@material-ui/core/TablePagination';
+import {
+    Dropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem,
+} from 'reactstrap';
+import { Link } from 'react-router-dom';
 import { Table } from 'reactstrap';
+import Card from '../../elements/Card/Card';
 import Styled from 'styled-components';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchGetTopic, fetchMultipleDeleteTopics } from '../../store/actions';
-import DeleteIcon from '@material-ui/icons/Delete';
-import DehazeIcon from '@material-ui/icons/Dehaze';
-import moment from 'moment';
-import { Input, Th,  lg } from '../../elements/Styled/StyledForm';
 
+import DehazeIcon from '@material-ui/icons/Dehaze';
+import { Input, Th, Overflow, md } from '../../elements/Styled/StyledForm';
+import moment from 'moment';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import CreateIcon from '@material-ui/icons/Create';
+import DescriptionIcon from '@material-ui/icons/Description';
 // --- Elements, Pages, Components --- //
+import {
+    fetchGetTopic,
+    fetchMultipleDeleteTopics,
+    // fetchMultipleClone,
+    // fetchFindProduct,
+    fetchPostTopic,
+} from '../../store/actions';
 import AddNewTopic from './AddNewTopic';
 import UpdateTopic from './UpdateTopic';
 import DeleteTopic from './DeleteTopic';
-import Card from '../../elements/Card/Card';
 
 // --- Styled Components --- //
 
-const SectionOne = Styled.div`
-    margin: ${lg} 0;
-    display: flex;
-    justify-content: space-between;
+const ButtonLink = Styled.button`
+    background-color:${(props) => (props.detail ? 'grey' : '#0098DA')};
+    padding: 5px;
+    border-radius: 3px;
+    color: white;
+    font-size: ${md};
+    border: 1px solid #ced4da;
+    font-Weight: 400;
 `;
-// --- Batas --- //
 
 const DataTopic = (props) => {
     const dispatch = useDispatch();
     const topic = useSelector((state) => state.topic.getTopic);
     console.log(topic);
 
-    // --- useEffect --- Get Data topic ---//
-    useEffect(() => {
-        dispatch(fetchGetTopic());
-    }, [dispatch]);
+    // --- PAGINATION --- //
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(5);
 
-    // --- For Multiple Delete --- //
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
+
+    // --- Dropdown --- //
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+    const toggle = () => setDropdownOpen((prevState) => !prevState);
+
     const [form, setForm] = useState({
         id: [],
     });
+
+    const [searching, setSearching] = useState({
+        search: '',
+    });
+    console.log(searching, 'pen tau');
+    // --- useEffect --- Get Data Topic ---//
+    useEffect(() => {
+        dispatch(fetchGetTopic());
+    }, [dispatch]);
 
     // --- handleCheckboxChange --- //
     const handleCheckboxChange = (event) => {
@@ -54,85 +93,234 @@ const DataTopic = (props) => {
         event.preventDefault();
         dispatch(fetchMultipleDeleteTopics(form));
     };
+
+    // --- Multiple Clone --- //
+    // const handlleMultipleClone = (event) => {
+    //     event.preventDefault();
+    //     dispatch(fetchMultipleCloneProduct(form));
+    // };
+
+    // --- handle Change --- //
+    const handleChange = (event) => {
+        setSearching({ ...searching, [event.target.name]: event.target.value });
+    };
+
+    // const handleSearch = (event) => {
+    //     event.preventDefault();
+    //     dispatch(fetchFindProduct(searching));
+    // };
+
     return (
         <React.Fragment>
-            {/* --- section 1 --- Add New Topic and Search Topic --- */}
-            <SectionOne>
-                <AddNewTopic />
-                <div>
-                    <label>Search</label> <Input type="search" />
-                </div>
-            </SectionOne>
+            {/* --- section 1 --- Button Action link to Add Product ---*/}
+            <div
+                style={{
+                    margin: '20px 0',
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                }}
+            >
+                {form.id[0] ? (
+                    <Dropdown size="sm" isOpen={dropdownOpen} toggle={toggle}>
+                        <DropdownToggle
+                            style={{ backgroundColor: '#0098DA' }}
+                            caret
+                        >
+                            Actions
+                        </DropdownToggle>
+                        <DropdownMenu>
+                            <DropdownItem onClick={handlleMultipleDelete}>
+                                Delete
+                            </DropdownItem>
+                            {/* <DropdownItem onClick={handlleMultipleClone}>
+                                Clone
+                            </DropdownItem> */}
+                        </DropdownMenu>
+                    </Dropdown>
+                ) : (
+                    <Dropdown size="sm" isOpen={dropdownOpen} toggle={toggle}>
+                        {' '}
+                        <DropdownToggle
+                            style={{ backgroundColor: '#0098DA' }}
+                            caret
+                            disabled
+                        >
+                            Actions
+                        </DropdownToggle>
+                    </Dropdown>
+                )}
 
-            {/* --- section 2 --- Table Get Data Product In Table --- */}
+                <div>
+                    <label>Search</label>{' '}
+                    <Input
+                        type="search"
+                        name="search"
+                        value={searching.search}
+                        onChange={handleChange}
+                    />
+                </div>
+                {/* <input type="button" onClick={handleSearch} value="KLIK" /> */}
+            </div>
+
+            {/* --- section 2 --- Get Data Product --- */}
             <Card isNormal>
-                <Table>
-                    <thead>
-                        <tr>
-                            <Th>
-                                <div>
-                                    {form.id[0] ? (
-                                        <div onClick={handlleMultipleDelete}>
-                                            <DeleteIcon color="error" />
-                                        </div>
-                                    ) : (
-                                        <DehazeIcon />
-                                    )}
-                                </div>
-                            </Th>
-                            <Th>Name</Th>
-                            <Th>Slug</Th>
-                            <Th>Created At</Th>
-                            <Th>Updated At</Th>
-                            <Th>Actions</Th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {topic !== null &&
-                            topic.data.map((item) => {
-                                return (
-                                    <tr key={item._id}>
+                {/* --- untuk hapus melalui button --- */}
+                <Overflow>
+                    {/* ------ jika product !== null return hasil get product jika masih nulltampilkan loading,
+                     di dalam product apabila ternyata data.lentgh < 0 maka tampilkan table kosong -------*/}
+                    {topic === null ? (
+                        <React.Fragment>
+                            <Table>
+                                <thead>
+                                    <tr>
                                         <Th>
-                                            <Input
-                                                checkbox
-                                                type="checkbox"
-                                                id={item._id}
-                                                value={item._id}
-                                                onChange={handleCheckboxChange}
-                                            />
+                                            <DehazeIcon />
                                         </Th>
-                                        <Th as="td" td>
-                                            {item.name}
-                                        </Th>
-                                        <Th as="td" td>
-                                            {item.slug}
-                                        </Th>
-                                        <Th as="td" td>
-                                            {moment(item.created_at).format(
-                                                'MMMM Do YYYY, h:mm:ss a'
-                                            )}
-                                        </Th>
-                                        <Th as="td" td>
-                                            {moment(item.updated_at).format(
-                                                'MMMM Do YYYY, h:mm:ss a'
-                                            )}
-                                        </Th>
-                                        <Th as="td" td>
-                                            <div
-                                                style={{
-                                                    display: 'flex',
-                                                    flexDirection: 'row',
-                                                }}
-                                            >
-                                                <UpdateTopic id={item._id} />
-                                                <DeleteTopic id={item._id} />
-                                            </div>
+                                        <Th>Name</Th>
+                                        <Th>Slug</Th>
+                                        <Th>Created At</Th>
+                                        <Th>Update At</Th>
+                                        <Th style={{ width: '100px' }}>
+                                            Actions
                                         </Th>
                                     </tr>
-                                );
-                            })}
-                    </tbody>
-                </Table>
+                                </thead>
+                            </Table>
+                            <div
+                                style={{
+                                    textAlign: 'center',
+                                    padding: '100px',
+                                }}
+                            >
+                                Loading ...
+                            </div>
+                        </React.Fragment>
+                    ) : topic.data.length >= 1 ? (
+                        <Table>
+                            <thead>
+                                <tr>
+                                    <Th>
+                                        <DehazeIcon />
+                                    </Th>
+                                    <Th>Name</Th>
+                                    <Th>Slug</Th>
+                                    <Th>Created At</Th>
+                                    <Th>Update At</Th>
+                                    <Th style={{ width: '100px' }}>Actions</Th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {/* {(rowsPerPage > 0
+            ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+            : rows
+          ).map((row)  */}
+                                {topic.data
+                                    .slice(
+                                        page * rowsPerPage,
+                                        page * rowsPerPage + rowsPerPage
+                                    )
+                                    .map((item) => {
+                                        return (
+                                            <tr key={item._id}>
+                                                <Th>
+                                                    <Input
+                                                        checkbox
+                                                        type="checkbox"
+                                                        id={item._id}
+                                                        value={item._id}
+                                                        onChange={
+                                                            handleCheckboxChange
+                                                        }
+                                                    />
+                                                </Th>
+
+                                                <Th as="td" td>
+                                                    {item.name}
+                                                </Th>
+                                                <Th as="td" td>
+                                                    {item.slug}
+                                                </Th>
+                                                <Th as="td" td>
+                                                    {moment(
+                                                        item.created_at
+                                                    ).format(
+                                                        'MMMM Do YYYY, h:mm:ss a'
+                                                    )}
+                                                </Th>
+                                                <Th as="td" td>
+                                                    {moment(
+                                                        item.updated_at
+                                                    ).format(
+                                                        'MMMM Do YYYY, h:mm:ss a'
+                                                    )}
+                                                </Th>
+
+                                                <Th as="td" td>
+                                                    <div
+                                                        style={{
+                                                            display: 'flex',
+                                                            flexDirection:
+                                                                'row',
+                                                        }}
+                                                    >
+                                                        <UpdateTopic
+                                                            id={item._id}
+                                                        />
+                                                        <DeleteTopic
+                                                            id={item._id}
+                                                        />
+                                                    </div>
+                                                </Th>
+                                            </tr>
+                                        );
+                                    })}
+                            </tbody>
+                            <tfoot>
+                                <tr>
+                                    <TablePagination
+                                        rowsPerPageOptions={[5, 10, 15]}
+                                        count={
+                                            topic !== null && topic.data.length
+                                        }
+                                        rowsPerPage={rowsPerPage}
+                                        page={page}
+                                        onChangePage={handleChangePage}
+                                        onChangeRowsPerPage={
+                                            handleChangeRowsPerPage
+                                        }
+                                    />
+                                </tr>
+                            </tfoot>
+                        </Table>
+                    ) : (
+                        <React.Fragment>
+                            <Table>
+                                <thead>
+                                    <tr>
+                                        <Th>
+                                            <DehazeIcon />
+                                        </Th>
+                                        <Th>Name</Th>
+                                        <Th>Slug</Th>
+                                        <Th>Created At</Th>
+                                        <Th>Update At</Th>
+                                        <Th style={{ width: '100px' }}>
+                                            Actions
+                                        </Th>
+                                    </tr>
+                                </thead>
+                            </Table>
+                            <div
+                                style={{
+                                    textAlign: 'center',
+                                    padding: '100px',
+                                }}
+                            >
+                                You have no topic in this date range.
+                            </div>
+                        </React.Fragment>
+                    )}
+                </Overflow>
             </Card>
         </React.Fragment>
     );
