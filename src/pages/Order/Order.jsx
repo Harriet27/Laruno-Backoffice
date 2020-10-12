2d551472fae24b501a3fbd726d9b07fa2993da0e
agusTrih <ant3dong@gmail.com>
2020-10-12 15:28:13 +0700

    orders

--- src/pages/Order/Order.jsx
+++ src/pages/Order/Order.jsx
@@ -1,9 +1,76 @@
 import React from 'react';
+import Styled from 'styled-components';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { faWallet } from '@fortawesome/free-solid-svg-icons';
+
+import { useSelector } from 'react-redux';
+import CardGetData from '../../elements/Card/CardGetData';
 import DataOrder from '../../components/OrderOnline/DataOrder';
+
+const Wraps = Styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    margin: 20px 0;
+`;
 export default function Order() {
+    const order = useSelector((state) => state.orders.getOrders);
     return (
-        <div style={{ margin: '50px' }}>
+        <section style={{ margin: '50px' }}>
+            {order === null ? (
+                <Wraps>
+                    <CardGetData
+                        icon={faShoppingCart}
+                        number="0"
+                        text="Total Orders"
+                    ></CardGetData>
+
+                    <CardGetData
+                        icon={faWallet}
+                        number="2"
+                        text="Total Paid"
+                    ></CardGetData>
+
+                    <CardGetData
+                        icon={faShoppingCart}
+                        number="0"
+                        text="Unpaid Orders"
+                    ></CardGetData>
+
+                    <CardGetData
+                        icon={faShoppingCart}
+                        number="0"
+                        text="Unpaid Orders"
+                    ></CardGetData>
+                </Wraps>
+            ) : (
+                <Wraps>
+                    <CardGetData
+                        icon={faShoppingCart}
+                        number={order.data.length}
+                        text="Total Orders"
+                    ></CardGetData>
+
+                    <CardGetData
+                        icon={faWallet}
+                        number="2"
+                        text="Total Paid"
+                    ></CardGetData>
+
+                    <CardGetData
+                        icon={faShoppingCart}
+                        number="0"
+                        text="Unpaid Orders"
+                    ></CardGetData>
+
+                    <CardGetData
+                        icon={faShoppingCart}
+                        number="0"
+                        text="Unpaid Orders"
+                    ></CardGetData>
+                </Wraps>
+            )}
             <DataOrder />
-        </div>
+        </section>
     );
 }
