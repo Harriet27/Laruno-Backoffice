d1595aca3606e641e68c936041b7d0a5a144cf2e
agusTrih <ant3dong@gmail.com>
2020-11-22 16:26:24 +0700

    fix bug multiple actions

--- src/elements/MultipleActions/MultipleActions.js
+++ src/elements/MultipleActions/MultipleActions.js
@@ -32,7 +32,7 @@ export default function MultipleActions(props) {
 }
 
 MultipleActions.propTypes = {
-  isLogic: PropTypes.any.isRequired,
+  isLogic: PropTypes.any,
   handleDelete: PropTypes.any,
   handleClone: PropTypes.any,
 };
