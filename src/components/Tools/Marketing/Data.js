356896937ecb7e8f8a4c29b5acfd4f6b2de6eb9f
Agustrihanton <agustrihanton97@gmail.com>
2020-11-30 10:12:28 +0700

    fix bug image

--- src/components/Tools/Marketing/Data.js
+++ src/components/Tools/Marketing/Data.js
@@ -2,12 +2,12 @@ const DataImage = [
   {
     name: 'Bussiness revolution and tranformation 4.0',
     url:
-      'https://lh3.googleusercontent.com/proxy/emUHLxLg8BiYrT-v0MoXF2i_WEnVuuHFTRglcJsY_nvToQq7isglAYFC2P6Lp-cAexldIfc3zovCv3SruOzwzH1_RT9g0ws0jJivrtMnioSwaA',
+      'https://lh3.googleusercontent.com/proxy/iLLddlk2OZCs-EMC0joH5qjGlwrLkLU_6-QxJnX4HGvm_qY4YiQ1ZLsz-yBFKNSA2Do1VcQSId4TZ3V9O1cMrGNcK8wZy0ASO5j78m5ILCoP7H_GNnoNc3FmS-S2ejf5vl4x4CK2I3rw',
   },
   {
     name: 'Bussiness revolution and tranformation 4.0',
     url:
-      'https://lh3.googleusercontent.com/proxy/emUHLxLg8BiYrT-v0MoXF2i_WEnVuuHFTRglcJsY_nvToQq7isglAYFC2P6Lp-cAexldIfc3zovCv3SruOzwzH1_RT9g0ws0jJivrtMnioSwaA',
+      'https://lh3.googleusercontent.com/proxy/iLLddlk2OZCs-EMC0joH5qjGlwrLkLU_6-QxJnX4HGvm_qY4YiQ1ZLsz-yBFKNSA2Do1VcQSId4TZ3V9O1cMrGNcK8wZy0ASO5j78m5ILCoP7H_GNnoNc3FmS-S2ejf5vl4x4CK2I3rw',
   },
 ];
 
