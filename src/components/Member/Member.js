ec53df3bf7709eca20f87a8bbf701801a6c26f0a
Agustrihanton <agustrihanton97@gmail.com>
2020-11-30 15:48:48 +0700

    set up member

---
+++ src/components/Member/Member.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ProfileMember from './ProfileMember';
+
+export default function Member() {
+  return (
+    <div>
+      <ProfileMember />
+    </div>
+  );
+}
---
+++ src/components/Member/ProfileMember.js
@@ -0,0 +1,10 @@
+// import { Card } from '@material-ui/core';
+import React from 'react';
+import Card from '../../elements/Card/Card';
+export default function ProfileMember() {
+  return (
+    <div>
+      <Card isNormal>height</Card>
+    </div>
+  );
+}
